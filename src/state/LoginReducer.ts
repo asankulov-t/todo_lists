a85a76fb892a3be40cc6c8d352368333098cc5a8
unknown <asankulov-t>
2023-09-19 19:55:45 +0600

    type Payload

--- src/state/LoginReducer.ts
+++ src/state/LoginReducer.ts
@@ -1,24 +1,18 @@
 import {LoginResType, loginType, TODOLISTAPI} from "../Api/Api";
 import {Dispatch} from "redux";
 import {actions, setStatusAc} from "./api_status";
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 let initState = {
     isLoggin: false
 }
-type actionType = {
-    type: 'LOGINED',
-    isLoggin: boolean
-}
-
-
 
 const slice = createSlice({
     name: "auth",
     initialState: initState,
     reducers: {
-        setLogginAc(state, action){
-            state.isLoggin=action.payload
+        setLogginAc(state, action:PayloadAction<{value:boolean}>){
+            state.isLoggin=action.payload.value
         }
     }
 })
@@ -31,16 +25,16 @@ export const LoginFetchTh = (data: loginType) => (dispatch: Dispatch) => {
     dispatch(setStatusAc(null, 'loading'))
     TODOLISTAPI.authAPI(data).then((r) => {
         if (r.data.resultCode === 0) {
-            dispatch(setLogginAc(true))
+            dispatch(setLogginAc({value:true}))
             dispatch(setStatusAc(null, 'succeess'))
         } else {
-            dispatch(setLogginAc(false))
+            dispatch(setLogginAc({value: false}))
             dispatch(setStatusAc(r.data.messages[0], 'failed'))
         }
 
     })
         .catch((error) => {
-            dispatch(setLogginAc(false))
+            dispatch(setLogginAc({value:false}))
             dispatch(setStatusAc(error.messages, 'failed'))
         })
 }
@@ -49,15 +43,15 @@ export const logoutTh = () => (dispatch: Dispatch) => {
     dispatch(setStatusAc(null, 'loading'))
     TODOLISTAPI.logoutApi().then((r) => {
         if (r.data.resultCode === 0) {
-            dispatch(setLogginAc(false))
+            dispatch(setLogginAc({value:true}))
             dispatch(setStatusAc(null, 'succeess'))
         } else {
-            dispatch(setLogginAc(false))
+            dispatch(setLogginAc({value:false}))
             dispatch(setStatusAc(r.data.messages[0], 'failed'))
         }
     })
         .catch((error) => {
-            dispatch(setLogginAc(false))
+            dispatch(setLogginAc({value:false}))
             dispatch(setStatusAc(error.messages, 'failed'))
         })
 }
